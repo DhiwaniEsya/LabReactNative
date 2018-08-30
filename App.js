a68ab260a754d4915e8b01c2383015d60308b352
Ramadhani Samudra Gawang Indiyanto Ramadhani Samudra Gawang Indiyanto <ganar.gatul@gmail.com>
2018-08-30 10:17:00 +0700

    Add feature component

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import Judul from './Components/Judul';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -23,6 +24,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+      <Judul/>
         <Text style={styles.instructions}>
         {instructions}
         </Text>
---
+++ Components/Judul.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+class Judul extends React.Component {
+	render() {
+		return (
+			<Text style= {salon.judul}>Biodata</Text>
+		)
+	}
+}
+
+	const salon = {
+		judul: {
+			color: "#FFFFFF",
+			fontSize: 75,
+			fontWeight:
+			'bold'
+		}
+	}
+
+
+export default Judul;
